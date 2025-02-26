5228d87c18d49e7753c57c7403754710b3eee488
arthur282 <luisarturocruz57@gmail.com>
2025-02-26 18:29:35 -0400

    asignacion de autoridad completado

--- modules/sgu/models/sgu_property.py
+++ modules/sgu/models/sgu_property.py
@@ -4,25 +4,19 @@ class SguAutoridad(models.Model):
     _name = 'sgu_autoridad'
     _description = 'Autoridades de la institución'
 
-    autoridad = fields.Many2one(
-        'sgu_tipo_autoridad',
-        string='Tipo de autoridad',
-        ondelete='cascade'
-        
-    )
-    institucion = fields.Many2one(
-        'sgu_instituto',
-        string='Institución',
-        ondelete='cascade'
-    )
+    autoridad = fields.Many2one('sgu_tipo_autoridad', string='Tipo de autoridad')
+    institucion = fields.Many2one('sgu_instituto', string='Institución')
     nombre = fields.Char(string='Nombre')
     firmaDigital = fields.Image(string='Firma Digital')
+    active = fields.Boolean(default=True ,string='Estatus')
 
 class SguTipoAutoridad(models.Model):
     _name = 'sgu_tipo_autoridad'
     _description = 'Tipos de autoridades de la institución'
+    _rec_name = 'tipo'
 
     tipo = fields.Char(string='Tipo de autoridad')
+    active = fields.Boolean(default=True)
 
 class SguCarreras(models.Model):
     _name = 'sgu_carreras'
@@ -35,10 +29,12 @@ class SguCarreras(models.Model):
         ('semestral', 'Semestral'),
         ('trimestral', 'Trimestral')
     ], string='Modalidad')
+    active = fields.Boolean(default=True)
 
 class SguInstituto(models.Model):
     _name = 'sgu_instituto'
-    _description = 'Institutos de la universidad'   
+    _description = 'Institutos de la universidad'  
+    _rec_name = 'nombre' 
 
     codigo_opsu = fields.Integer(string='Código OPSU')
     nombre = fields.Char(string='Nombre')
@@ -46,3 +42,5 @@ class SguInstituto(models.Model):
     direccion = fields.Char(string='Dirección')
     telefono = fields.Integer(string='Teléfono')
     correo = fields.Char(string='Correo')
+    active = fields.Boolean(default=True)
+
